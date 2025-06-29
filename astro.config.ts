77fc98c651a6a8a20cd8130a0df209477d275021
Damir Modyarov <otomir@yandex.ru>
2025-06-29 03:53:56 +0300

    build: Setup Cloudflare adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
+import cloudflare from "@astrojs/cloudflare"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig, envField } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: cloudflare(),
+    output: "server",
     vite: {
         plugins: [tailwindcss()],
     },
